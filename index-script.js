187f05bc70b1fee53f6566a85e16cd16e0335426
Julien Labatut <j.labatut33127@gmail.com>
2023-04-18 14:12:30 +0200

    Create index script

---
+++ index-script.js
@@ -0,0 +1,53 @@
+const mainLogo = document.getElementById('main-logo');
+
+// Définition du Cookie
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+// Suppression du Cookie
+function deleteCookie(cname) {
+    const d = new Date();
+    d.setTime(d.getTime() + (24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=;" + expires + ";path=/";
+}
+  
+// Lecture du Cookie
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+  
+// Définition du Consentement Utilisateur
+function acceptCookieConsent(){
+    deleteCookie('user_cookie_consent');
+    setCookie('user_cookie_consent', 1, 30);
+    document.getElementById("cookieNotice").style.display = "none";
+}
+  
+// Ecouteur d'évènement
+let cookie_consent = getCookie("user_cookie_consent");
+if (cookie_consent != ""){
+    document.getElementById("cookieNotice").style.display = "none";
+} else {
+    document.getElementById("cookieNotice").style.display = "block";
+}
+
+window.onload = () => {
+    mainLogo.setAttribute('class', 'non-transparent main-logo');
+}
